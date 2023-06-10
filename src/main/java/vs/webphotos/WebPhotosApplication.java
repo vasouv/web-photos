090fe3862505af01fbb3b064fdeb1b36a847e755
Vasilis Souvatzis <vasouv@gmail.com>
2023-06-10 20:50:56 +0300

    Initial commit

---
+++ src/main/java/vs/webphotos/WebPhotosApplication.java
@@ -0,0 +1,13 @@
+package vs.webphotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPhotosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPhotosApplication.class, args);
+    }
+
+}
